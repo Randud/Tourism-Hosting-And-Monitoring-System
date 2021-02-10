b8c02ca2e82417787bda0a1715dcc889f611f2d7
Randud <randu612@gmail.com>
2021-02-10 14:08:44 +0530

    Frontend auth added

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const touristRoutes = require('./api/routes/tourist');
 const helpCenterRoutes = require('./api/routes/helpcenter');
@@ -18,6 +19,8 @@ mongoose.connect('mongodb+srv://dbUser:dbUser123@afcluster.xlexh.mongodb.net/tou
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
 
 //handle CORS errors
 app.use((req, res, next) => {
@@ -29,7 +32,7 @@ app.use((req, res, next) => {
 
 app.use('/tourist', touristRoutes);
 app.use('/helpCenter', helpCenterRoutes);
-app.use('/auth', auth);
+app.use('/api/auth', auth);
 
 app.use((req, res, next) => {
     const error = new Error("not found!");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,39 @@
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Test</h1>
-    </div>
-  );
+import Home from "./pages/Home";
+import Routes from "./utils/routing/Routes";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Layout from "./hoc/Layout";
+import { check_authenticated } from "./actions/action.auth";
+import setAuthToken from "./utils/setAuthToken";
+
+//Check the token
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  //Setting up token
+  useEffect(() => {
+    store.dispatch(check_authenticated());
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={Routes} />
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
